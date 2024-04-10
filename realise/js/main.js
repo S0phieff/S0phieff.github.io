2dfeaf5d7643df834eb04872dbdbef0ba96960ba
S0phieff <159007109+S0phieff@users.noreply.github.com>
2024-04-10 10:17:07 +0200

    Add files via upload

---
+++ realise/js/main.js
@@ -0,0 +1,18 @@
+// VARIABLES GLOBALES
+let exemplevariable = "hello world"
+
+// la page HTML a fini de charger, on peut donc
+// interagir avec via des scripts sans risque d'erreur
+document.addEventListener("DOMContentLoaded", function() {
+    // laisser un message dans la console du navigateur
+    console.log('ready');
+
+    // lancer une fonction externe
+    exemplefonction();
+});
+
+// FUNCTIONS EXTERNES
+function exemplefonction() {
+    // utiliser une variable pour afficher son contenu dans la console
+    console.log(exemplevariable);
+}
